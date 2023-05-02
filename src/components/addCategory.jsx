aaf5c566277dc93010505ad666563f23375e3f94
Santiago Quintero <san1209q@gmail.com>
2023-05-02 11:29:24 -0500

    first commit

---
+++ src/components/addCategory.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+export const AddCategory = ({onNewCategory}) => {
+
+const [inputValue, setInputValue] = useState('')
+
+          const onInputChange = (event)=>{setInputValue(event.target.value)}
+      
+          const onSubmit = (event) => {
+                event.preventDefault();
+             if(inputValue.trim().length <= 1) return;
+                setInputValue('')
+                onNewCategory(inputValue.trim())
+          }
+
+ return (
+    <form onSubmit= {onSubmit}>
+        <input type="text"
+               placeholder="buscar gif"  
+               value={inputValue}
+               onChange={onInputChange}
+                />
+        </form>
+         )
+}
+
+
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export * from './addCategory'
+export * from './GifGrid';
+export * from './GifItem';
